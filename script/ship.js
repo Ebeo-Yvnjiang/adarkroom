1eddd390213f5f2eed15d4b9e5bfbfbae93adbff
Billy2007 <54059423+Ebeo-Yvnjiang@users.noreply.github.com>
2020-06-11 12:38:41 +0800

    Update ship.js

--- script/ship.js
+++ script/ship.js
@@ -2,8 +2,8 @@
  * Module that registers the starship!
  */
 var Ship = {
-	LIFTOFF_COOLDOWN: 120,
-	ALLOY_PER_HULL: 1,
+	LIFTOFF_COOLDOWN: 12,
+	ALLOY_PER_HULL: 100,
 	ALLOY_PER_THRUSTER: 1,
 	BASE_HULL: 0,
 	BASE_THRUSTERS: 1,
